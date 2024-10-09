a89cd0d6f70e0dc66050457801eaa7887722669b
Tim <36207941+jianggl2000@users.noreply.github.com>
2024-10-09 17:26:28 -0400

    Update get_promoter_from_GTF.sh

--- get_promoter_from_GTF.sh
+++ get_promoter_from_GTF.sh
@@ -91,3 +91,54 @@ $3 == "gene" {
     print gene_name[2], chr, promoter_start, promoter_end, strand
 }' $GTF | tee promoters.saf | \
   awk '{print $2,$3-1,$4,$1,".",$5}' | sed '1d' > promoters.bed
+
+
+
+
+
+############################################################################################################
+############################################################################################################
+############################################################################################################
+#this script extract promoter for each transcript(need more work)
+
+awk -v upstream=$upstream -v downstream=$downstream \
+'BEGIN {
+    OFS="\t"
+    print "GeneID\tChr\tStart\tEnd\tStrand"
+}
+
+$3 == "transcript" {
+    split($0, a, "\t")
+    chr = $1
+    start = $4
+    end = $5
+    strand = $7
+    skip=""
+    
+    split(a[9], attrs, ";")
+    for (i in attrs) {
+        if (attrs[i] ~ /gene_type/) {
+            split(attrs[i], gene_type, " ")
+            if (gene_type[2] ~ /protein_coding/)
+                skip="FALSE"
+        }
+        if (attrs[i] ~ /transcript_id/) {
+            split(attrs[i], transcript_id, " ")
+            gsub(/"/, "", transcript_id[2])
+            break
+        }
+    }
+    
+    if (strand == "+") {
+        promoter_start = start - upstream
+        promoter_end = start + downstream
+    } else if (strand == "-") {
+        promoter_start = end - downstream
+        promoter_end = end + upstream
+    }
+    
+    if (promoter_start < 1) promoter_start = 1
+    
+    if(skip=="FALSE") print transcript_id[2], chr, promoter_start, promoter_end, strand
+}' $GTF | tee promoters.transcript.saf | \
+  awk '{print $2,$3-1,$4,$1,".",$5}' | sed '1d' > promoters.transcript.bed
